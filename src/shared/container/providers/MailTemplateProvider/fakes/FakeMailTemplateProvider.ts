720ad704d6f4156a36631aaffd2feddf0ef28064
Frederico Ferrarini <fred.ferrarini@gmail.com>
2020-11-24 07:07:53 +0100

    tests refactoring

--- src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
+++ src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
@@ -1,9 +1,8 @@
 import IMailTemplateProvider from '../models/IMailTemplateProvider';
-import IParseMailProviderDTO from '../dtos/IParseMailTemplateDTO';
 
 class FakeMailTemplateProvider implements IMailTemplateProvider {
-  public async parse({ template }: IParseMailProviderDTO): Promise<string> {
-    return template;
+  public async parse(): Promise<string> {
+    return 'Mail content';
   }
 }
 
